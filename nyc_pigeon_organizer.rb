a3c2c290af990a3cbce875d6606c1bf7f38367f6
Laura Archambault <laarchambault@gmail.com>
2019-11-09 01:17:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,7 +19,7 @@ def nyc_pigeon_organizer(data)
   pigeon_names = create_keys_for_pigeons(data)
 
   data.each { |attribute_group_key, attribute_array|
-  attribute_group_key.each { |attribute_type_key, value_array|
+  attribute_array.each { |attribute_type_key, value_array|
   binding.pry
   value_array.any? { |x| 
   if !pigeon_names.include? x
